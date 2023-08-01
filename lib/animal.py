a5c9aca535ba656ac1b655ebfb383bce287e2eba
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-08-01 18:59:44 -0400

    partial solution without testing files

--- lib/animal.py
+++ lib/animal.py
@@ -1,2 +1,35 @@
 class Animal:
-    pass
+    all_animals = []  # Class variable to store all animal instances
+
+    def __init__(self, species, nickname, weight, zoo):
+        self.species = species
+        self._nickname = nickname
+        self._weight = weight
+        self.zoo = zoo
+        Animal.all_animals.append(self)  # Add the current instance to the list of all animals
+
+    @property
+    def nickname(self):
+        return self._nickname
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, new_weight):
+        self._weight = new_weight
+
+    def get_species(self):
+        return self.species
+
+    def get_zoo(self):
+        return self.zoo
+
+    @classmethod
+    def all(cls):
+        return cls.all_animals
+
+    @classmethod
+    def find_by_species(cls, species):
+        return [animal for animal in cls.all_animals if animal.species == species]
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,4 +1,76 @@
+from typing import Any
 from lib.animal import Animal
 
 class Zoo:
-    pass
+
+    all = []
+
+    def __init__(self, name, location):
+        self._name = name
+        self._location = location
+        self._animals = []
+        Zoo.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def location(self):
+        return self._location
+    
+    def animals(self):
+        return self._animals
+    
+    def add_animals(self, animal):
+        return self._animals.append(animal)
+    
+    def animal_species(self):
+        species_set = set()
+        for animal in self._animals:
+            species_set.add(animal)
+        return list(species_set)
+    
+    def get_species(self, species):
+        return [animal for animal in self._animals if animal.species == species]
+    
+
+    def get_location(cls, location):
+        return [zoo for zoo in cls.all if zoo.location == location]
+
+    
+
+# Creating zoo instances
+zoo1 = Zoo("Zoo A", "Location A")
+zoo2 = Zoo("Zoo B", "Location B")
+
+# Creating animals and adding them to zoos
+animal1 = Animal("Dog", "Buddy")
+animal2 = Animal("Cat", "Whiskers")
+animal3 = Animal("Dog", "Max")
+animal4 = Animal("Elephant")
+
+zoo1.add_animal(animal1)
+zoo1.add_animal(animal2)
+zoo2.add_animal(animal3)
+zoo2.add_animal(animal4)
+
+# Accessing the attributes and methods
+print("Zoo 1 Name:", zoo1.get_name())
+print("Zoo 1 Location:", zoo1.get_location())
+print("Zoo 1 Animals:", [animal.species for animal in zoo1.animals()])
+print("Zoo 1 Animal Species:", zoo1.animal_species())
+print("Zoo 1 Animals with species 'Dog':", [animal.species for animal in zoo1.find_by_species("Dog")])
+print("Zoo 1 Animal Nicknames:", zoo1.animal_nicknames())
+
+print("Zoo 2 Name:", zoo2.get_name())
+print("Zoo 2 Location:", zoo2.get_location())
+print("Zoo 2 Animals:", [animal.species for animal in zoo2.animals()])
+print("Zoo 2 Animal Species:", zoo2.animal_species())
+print("Zoo 2 Animals with species 'Dog':", [animal.species for animal in zoo2.find_by_species("Dog")])
+print("Zoo 2 Animal Nicknames:", zoo2.animal_nicknames())
+
+# Using the class method to find zoos by location
+zoos_in_location_b = Zoo.find_by_location("Location B")
+for zoo in zoos_in_location_b:
+    print("Zoo Name:", zoo.get_name(), "| Zoo Location:", zoo.get_location())
